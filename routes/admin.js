3269f7988f0ef0f93e29ba71cf30c25223c2623c
dehui333 <dehui333@gmail.com>
2019-02-17 11:40:02 +0800

    uses dotenv

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const { Pool } = require('pg')
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: '********',
-  port: 5432,
-})
+	connectionString: process.env.DATABASE_URL
+});
+
 
 /* SQL Query */
 var sql_query = 'SELECT * FROM users';
