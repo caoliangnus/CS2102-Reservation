679b8de683bba41b0dab8541605b173d60219271
tianlan <e0032176@u.nus.edu>
2019-02-17 16:28:02 +0800

    searh result page

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ var cSignupRouter = require('./routes/cSignup');    //Customer Signup page
 var rSignupRouter = require('./routes/rSignup');    //Restaurant Signup page
 var logInRouter = require('./routes/login');        //Login Page
 var manageBookingRouter = require('./routes/manageBooking');  //Manage Customer Booking Page
+var searchResultRouter = require('./routes/searchResult'); //Customer Search Result Page
 
-
+// Given Examples
 var tableRouter = require('./routes/table');
 var loopsRouter = require('./routes/loops');
 var selectRouter = require('./routes/select');
@@ -39,7 +40,9 @@ app.use('/cSignup', cSignupRouter);     //url: customer signup
 app.use('/rSignup', rSignupRouter);     //url: restaurant signup
 app.use('/login', logInRouter);         //url: login
 app.use('/manageBooking', manageBookingRouter); //url: manage booking
+app.use('/searchResult', searchResultRouter); //url: search result
 
+//example given
 app.use('/table', tableRouter);
 app.use('/loops', loopsRouter);
 app.use('/select', selectRouter);
---
+++ routes/searchResult.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+/* SQL Query */
+var sql_query = 'SELECT * FROM student_info';
+router.get('/', function (req, res, next) {
+    pool.query(sql_query, (err, data) => {
+        res.render('searchResult', { title: 'Search Result', data: data.rows });
+    });
+});
+
+module.exports = router;
